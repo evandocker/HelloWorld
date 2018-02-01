816987e2f04a4f2445c4fd4753b9c839bf8220e2
evanmin <282732437@qq.com>
2018-02-01 17:26:33 +0800

    First commit maven HelloWorld

---
+++ HelloWorld/src/main/java/com/evan/learning/HelloWorld/App.java
@@ -0,0 +1,16 @@
+package com.evan.learning.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void sayHello() {
+		System.out.println( "sayHello:Hello World! Evan Min" );
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Main:Hello World! Evan Min" );
+    }
+}
